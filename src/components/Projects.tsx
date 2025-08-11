33dd3fc029c98675ffed629df6ca0b1bb821fc84
BrunoAlves22 <brunoalves220701@gmail.com>
2025-08-11 20:10:25 -0300

    feito a seção de projetos

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,16 +1,40 @@
+"use client";
+
 import { ArrowRight } from "lucide-react";
 import { ProjectDetails } from "./project-details";
+import { useState } from "react";
+import { motion, useMotionValue, useSpring } from "motion/react";
 
 export function Projects() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [previewImage, setPreviewImage] = useState<string | null>(null);
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const springX = useSpring(x, { damping: 10, stiffness: 50 });
+  const springY = useSpring(y, { damping: 10, stiffness: 50 });
+  const handleMouseMove = (
+    event: React.MouseEvent<HTMLElement, MouseEvent>
+  ) => {
+    x.set(event.clientX + 20);
+    y.set(event.clientY + 20);
+  };
+
   return (
-    <section className="sm:px-10 px-5 lg:px-15 min-h-screen mt-20 md:mt-30">
+    <section
+      onMouseMove={handleMouseMove}
+      className="relative sm:px-10 px-5 lg:px-15 min-h-screen mt-20 md:mt-30"
+    >
       <h2 className="font-bold text-3xl md:text-4xl font-poppins text-white">
         Meus Projetos
       </h2>
 
       <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full" />
 
-      <div className="flex-wrap items-center py-10 justify-between space-y-14 sm:flex sm:space-y-0 font-dm-sans">
+      <div
+        onMouseEnter={() => setPreviewImage("/assets/img-landing.png")}
+        onMouseLeave={() => setPreviewImage(null)}
+        className="flex-wrap items-center py-10 justify-between space-y-14 sm:flex sm:space-y-0 font-dm-sans"
+      >
         <div>
           <p className="text-2xl text-white">Forest Landing Page</p>
 
@@ -20,15 +44,26 @@ export function Projects() {
             <span>TailwindCSS</span>
           </div>
         </div>
-        <button className="text-white flex items-center gap-1 cursor-pointer hover:scale-105 transition-all">
+        <button
+          className="text-white flex items-center gap-1 cursor-pointer hover:scale-105 transition-all"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           Ver Mais
           <ArrowRight />
         </button>
 
-        <ProjectDetails />
+        {isOpen && <ProjectDetails setIsOpen={setIsOpen} isOpen={isOpen} />}
       </div>
 
       <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full" />
+
+      {previewImage && !isOpen && (
+        <motion.img
+          src={previewImage}
+          style={{ x: springX, y: springY }}
+          className="fixed top-0 left-0 z-50 object-contain h-64 rounded-lg shadow-lg pointer-events-none w-96"
+        />
+      )}
     </section>
   );
 }
--- src/components/project-details.tsx
+++ src/components/project-details.tsx
@@ -1,11 +1,27 @@
-import { X } from "lucide-react";
+import { ArrowUpRightFromSquare, X } from "lucide-react";
 import Image from "next/image";
+import { Dispatch, SetStateAction } from "react";
+import { motion } from "motion/react";
 
-export function ProjectDetails() {
+interface ProjectDetailsProps {
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  isOpen?: boolean;
+}
+
+export function ProjectDetails({ setIsOpen, isOpen }: ProjectDetailsProps) {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center w-full h-full overflow-hidden backdrop-blur-sm">
-      <div className="relative max-w-2xl border shadow-sm rounded-2xl bg-gradient-to-l from-neutral-800 to-neutral-900 border-white/10">
-        <button className="absolute p-2 rounded-sm top-5 right-5 bg-neutral-800 hover:bg-neutral-700 transition-colors text-white cursor-pointer">
+    <div className="fixed inset-0 z-50 flex items-center justify-center w-full h-full overflow-hidden backdrop-blur-sm px-2">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.5 }}
+        transition={{ duration: 0.3 }}
+        className="relative max-w-2xl border shadow-sm rounded-2xl bg-gradient-to-l from-neutral-800 to-neutral-900 border-white/10"
+      >
+        <button
+          className="absolute p-2 rounded-sm top-5 right-5 bg-neutral-800/50 hover:bg-neutral-800 transition-colors text-white cursor-pointer"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           <X />
         </button>
         <Image
@@ -22,14 +38,26 @@ export function ProjectDetails() {
           <p className="text-sm text-neutral-400 font-normal">
             A beautiful landing page showcasing a forest theme.
           </p>
-          <div className="flex items-center gap-2 mt-2">
-            <span className="text-sm text-neutral-400">Criado por:</span>
-            <span className="text-sm font-semibold text-white">
-              Bruno Alves
-            </span>
+          <div className="flex items-center justify-between gap-2 mt-2">
+            <div>
+              <span className="text-sm text-neutral-400">Criado por: </span>
+              <span className="text-sm font-semibold text-white">
+                Bruno Alves
+              </span>
+            </div>
+
+            <a
+              href="https://github.com/BrunoAlves"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-dm-sans bg-neutral-800 hover:bg-neutral-700 transition-colors text-white px-4 py-2 rounded-md text-sm flex items-center"
+            >
+              Ver projeto
+              <ArrowUpRightFromSquare className="inline ml-1.5 size-4" />
+            </a>
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
